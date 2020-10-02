db2b6508e805429d32b90189227eb3476228d6a3
Andrew Kachnic <ajkachnic@protonmail.com>
2020-10-02 13:39:45 -0400

    updated auth flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
 import { useAuthState } from 'react-firebase-hooks/auth'
 import firebase, { authProviders } from 'lib/client/firebase'
 import { loginWith } from 'lib/client/helpers'
-import { useRequireNoUser } from 'lib/client/hooks'
+import { useRedirect, useRequireNoUser } from 'lib/client/hooks'
 
 import { FC } from 'react'
 
 const Index: FC = () => {
   const [ user, loading ] = useAuthState(firebase.auth())
-  useRequireNoUser(user, loading, '/protected')
+  useRequireNoUser(user, loading, '/')
 
+  const login = async () => {
+    await loginWith(authProviders.google)()
+    useRedirect('/onboard')
+  }
+  
   return <>
     <h1>Landing Page</h1>
     <p>
-      This is the landing page for this Next.js TypeScript SWR Water.css Serverless Firebase Auth template.{' '}
-      Click the button below to get access to the protected page.
+      THis is a website
     </p>
-    <button onClick={loginWith(authProviders.google)}>Login with Google</button>
+    <button onClick={login}>Login with Google</button>
   </>
 }
 
