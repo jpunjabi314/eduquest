452e8b2b13a91c1a5f960d447b416e41b4955a68
Andrew Kachnic <ajkachnic@protonmail.com>
2020-10-04 11:35:23 -0400

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { FC } from 'react'
 
@@ -6,6 +7,16 @@ import 'styles/global.css'
 import 'styles/inter.css'
 import 'styles/variables.css'
 
-const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
-
+const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
+  
 export default App
